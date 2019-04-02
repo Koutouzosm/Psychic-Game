e40f3cea6cd3a958ae70d314b12385dc77a98ca8
Mano <Koutouzosm@gmail.com>
2019-04-02 09:44:44 -0400

    revised game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,11 @@
 // Create all my variables for game logic
 
 //wins (number)
-var wins = 0;
-console.log("this loaded");
-
+var userWin = 0;
 //losses (number)
-var losses = 0;
-
+var userLoss = 0;
 // guesses that are left (number)
 var guessesLeft = 10;
-
 //letters already guessed (array)
 var lettersGuessed = [];
 
@@ -18,10 +14,10 @@ var computerChoice = "abcdefghijklmnopqrstuvwxyz".split("");
 var computerGuess = "";
 
 //create all variables that reference specific parts of the page
-var userWin = document.getElementById("userWin");
-var userLoss = document.getElementById("userLoss");
-var guessesLeft = document.getElementById("guessesLeft");
-var guessedLetters = document.getElementById("guessedLetters");
+var $userWin = document.getElementById("userWin");
+var $userLoss = document.getElementById("userLoss");
+var $guessesLeft = document.getElementById("guessesLeft");
+var $guessedLetters = document.getElementById("guessedLetters");
 
 //create a function to start/reset the game
 function newGame() {
@@ -34,46 +30,44 @@ function newGame() {
   //randomly choose computer guess
   var randomCompChoice = [Math.floor(Math.random() * computerChoice.length)];
   computerGuess = computerChoice[randomCompChoice];
-
-  console.log(randomCompChoice);
-
+  console.log(randomCompChoice)
   alert("New Game Starting! Good Luck!!!");
 }
 
 document.onkeyup = function (event) {
   var userGuess = event.key;
-  console.log(event);
   //on every key press you want to push the user guess into the letters guessed array
   lettersGuessed.push(userGuess);
+  $guessedLetters.textContent = lettersGuessed.join(", ")
 
   //THIS IS YOUR WIN CONDITION
   // if userGuess === computerGuess...you win
 if (userGuess === computerGuess) {
     // increase wins by 1
-    wins++;
+    userWin++;
     // start a new game newGame()
   newGame();
     //you can update html here
-  userWin.push("")
+  $userWin.textContent = userWin;
   }
   else {
     guessesLeft--;
   //you also want to decrement your guessesLeft variable
-  userGuess.text = "guessesLeft";
+  $guessesLeft.textContent = guessesLeft;
   }
 
   //THIS IS YOUR LOSS CONDITION
   //since you are decrementing the guessesLeft on every keypress your loss condition is here
   if (guessesLeft === 0) {
     //increment losses
-    losses++;
+    userLoss++;
 
     // start a new game newGame()
     newGame();
 
     //you can update html here
   
-    userLoss.pusht("")
+    $userLoss.textContent = userLoss;
   }
 
 }
